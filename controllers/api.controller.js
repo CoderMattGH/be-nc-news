7897e474c1809b4cbf283b40c32efbb926fb3d51
GANDHIX <mattdixon2000@hotmail.com>
2024-05-28 20:57:12 +0100

    Refactored to remove /api model

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,11 +1,9 @@
-const apiModel = require('../models/api.model.js');
+const endpointsObj = require('../endpoints.json');
 
 const getEndpoints = (req, res, next) => {
   console.log("In getEndpoints() in api.controller!");
 
-  const endpointsObj = apiModel.selectEndpoints();
-
-  res.status(200).send(endpointsObj);
+  res.status(200).send(require('../endpoints.json'));
 };
 
 module.exports = { getEndpoints };
