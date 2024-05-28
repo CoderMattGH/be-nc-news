ad4d59de1da39aaa16fb12dab074774567d419b5
GANDHIX <mattdixon2000@hotmail.com>
2024-05-28 16:33:22 +0100

    Added selectArticles()

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,5 +1,30 @@
 const db = require('../db/connection.js');
 
+const selectArticles = () => {
+  console.log("In selectArticles() in articles.model!");
+
+  const queryStr =
+    `SELECT articles.author, articles.title, articles.article_id,
+      articles.topic, articles.created_at, articles.votes, articles.article_img_url, 
+      COUNT(comments.comment_id)::INT AS comment_count
+      FROM comments
+      JOIN articles ON comments.article_id = articles.article_id
+      GROUP BY articles.article_id
+      ORDER BY articles.created_at DESC;`;
+
+  return db
+    .query(queryStr)
+    .then((result) => {
+      const articles = result.rows;
+
+      if (!articles.length) {
+        return Promise.reject({status: 404, msg: "Articles not found!"});
+      } else {
+        return articles;
+      }
+    });
+};
+
 const selectArticleById = (articleId) => {
   console.log("In selectArticleById() in articles.model!");
 
@@ -15,4 +40,4 @@ const selectArticleById = (articleId) => {
     });
 };
 
-module.exports = { selectArticleById };
+module.exports = { selectArticles, selectArticleById };
