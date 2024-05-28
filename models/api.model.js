e7d7fb4f77c64d212623f80acb66bef96bb1c627
GANDHIX <mattdixon2000@hotmail.com>
2024-05-28 20:58:20 +0100

    Removed /api model

--- models/api.model.js
+++
@@ -1,9 +0,0 @@
-const endpointsObj = require('../endpoints.json');
-
-const selectEndpoints = () => {
-  console.log("In selectEndpoints() in api.model!");
-
-  return endpointsObj;
-};
-
-module.exports = { selectEndpoints };
