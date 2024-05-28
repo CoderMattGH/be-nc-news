06feff55d422d9ed301c7f85b472e3021ace8626
GANDHIX <mattdixon2000@hotmail.com>
2024-05-28 20:19:14 +0100

    Added endpoint for GET /api/articles/:article_id/comments

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const apiController = require('./controllers/api.controller.js');
 const topicsController = require('./controllers/topics.controller.js');
 const articlesController = require('./controllers/articles.controller.js');
+const commentsController = require('./controllers/comments.controller.js');
 
 app.get('/api', apiController.getEndpoints);
 
@@ -13,29 +14,27 @@ app.get('/api/articles', articlesController.getArticles);
 
 app.get('/api/articles/:article_id', articlesController.getArticleById);
 
+app.get('/api/articles/:article_id/comments', 
+  commentsController.getCommentsByArticleId);
+
 // Handle psql errors
 app.use((err, req, res, next) => {
   console.log("In psql error handler!");
 
-  if (err.code) {
-    if(err.code === '22P02')
-      res.status(400).send({msg: 'Bad request!'})
-  } else {
-    // Pass on to next error handler
-    next(err);
-  }
+  if(err.code === '22P02')
+    res.status(400).send({msg: 'Bad request!'})
+  else
+    next(err);      // Pass on to next error handler
 });
 
 // Handle misc. errors
 app.use((err, req, res, next) => {
   console.log("In misc. error handler!");
 
-  if (err.status && err.msg) {
+  if (err.status && err.msg)
     res.status(err.status).send({msg: err.msg});
-  } else {
-    // Pass on to next error handler
-    next(err);
-  }
+  else
+    next(err);      // Pass on to next error handler
 });
 
 module.exports = app;
