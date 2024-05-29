df4e4164ca466d59dbe6af1b05cfc430e93d09d0
GANDHIX <mattdixon2000@hotmail.com>
2024-05-29 18:26:22 +0100

    Added tests for DELETE /api/comments/:comment_id

--- __tests__/api-comments.test.js
+++ __tests__/api-comments.test.js
@@ -154,3 +154,26 @@ describe("POST /api/articles/:article_id/comments", () => {
         });
   });  
 });
+
+describe("DELETE /api/comments/:comment_id", () => {
+  test("Returns a 204 status code with no content", () => {
+    return request(app).delete('/api/comments/1').expect(204)
+        .then(({body}) => {
+          expect(body).toEqual({});
+        });
+  });
+
+  test("Returns a 404 when the comment_id does not exist", () => {
+    return request(app).delete('/api/comments/9999').expect(404)
+        .then(({body}) => {
+          expect(body.msg).toBe("Resource not found!");
+        });
+  });
+
+  test("Returns a 400 when the comment_id is not a number", () => {
+    return request(app).delete('/api/comments/banana').expect(400)
+        .then(({body}) => {
+          expect(body.msg).toBe("Bad request!");
+        });
+  });
+});
