cd35de48b742305c63b999ca2eba522ddc9d93b1
GANDHIX <mattdixon2000@hotmail.com>
2024-05-29 14:09:55 +0100

    Refactored to make use of checkValueExists() service function

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,16 +1,22 @@
 const commentsModel = require('../models/comments.model.js');
+const miscService = require('../services/misc.service.js');
 
 const getCommentsByArticleId = (req, res, next) => {
   console.log("In getCommentsByArticleId() in comments.controller!");
 
   const articleId = req.params.article_id;
-  commentsModel.selectCommentsByArticleId(articleId)
-    .then((comments) => {
-      res.status(200).send({comments});
-    })
-    .catch((err) => {
-      next(err);
-    });
+
+  // Check article_id exists
+  miscService.checkValueExists('articles', 'article_id', articleId)
+      .then(() => {
+        return commentsModel.selectCommentsByArticleId(articleId);        
+      })
+      .then((comments) => {
+        res.status(200).send({comments});  
+      })
+      .catch((err) => {
+        next(err);
+      });
 };
 
 const postCommentByArticleId = (req, res, next) => {
@@ -19,13 +25,21 @@ const postCommentByArticleId = (req, res, next) => {
   const articleId = req.params.article_id;
   const {username, body} = req.body;
 
-  commentsModel.createComment(articleId, username, body)
-    .then((comment) => {
-      res.status(200).send({comment});
-    })
-    .catch((err) => {
-      next(err);
-    });
+  // Check article_id exists
+  miscService.checkValueExists('articles', 'article_id', articleId)
+      .then(() => {
+        // Check username exists
+        return miscService.checkValueExists('users', 'username', username);
+      })
+      .then(() => {
+        return commentsModel.createComment(articleId, username, body);
+      })
+      .then((comment) => {
+        res.status(200).send({comment});
+      })
+      .catch((err) => {
+        next(err);
+      });
 };
 
 module.exports = {getCommentsByArticleId, postCommentByArticleId};
