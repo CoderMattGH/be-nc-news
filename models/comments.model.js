2ee77266c80fadc306b27d938bbd487a497db8e5
GANDHIX <mattdixon2000@hotmail.com>
2024-05-29 12:28:37 +0100

    Added createComment() function

--- models/comments.model.js
+++ models/comments.model.js
@@ -1,13 +1,14 @@
 const db = require('../db/connection.js');
 
 const articlesModel = require('./articles.model.js');
+const usersModel = require('./users.model.js');
 
 const selectCommentsByArticleId = (articleId) => {
   console.log("In selectCommentsByArticleId() in comments.model!");
 
   // First check article is valid and exists
   return articlesModel.selectArticleById(articleId)
-    .then(() => {   // Article exists!
+    .then(() => {
       return db.query(
           `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;`, 
           [articleId]);
@@ -20,4 +21,29 @@ const selectCommentsByArticleId = (articleId) => {
     });
 };
 
-module.exports = {selectCommentsByArticleId};
+const createComment = (articleId, author, body) => {
+  console.log("In createComment() in comments.model!");
+
+  // First check article is valid and exists
+  return articlesModel.selectArticleById(articleId)
+      .then(() => {
+        // Check username is valid and exists
+        return usersModel.selectUserByUsername(author);
+      })
+      .then(() => {
+        return db.query(
+          `INSERT INTO comments(article_id, author, body) 
+            VALUES ($1, $2, $3)
+            RETURNING *;`,
+          [articleId, author, body]
+        );
+      })
+      .then(({rows}) => {
+        return rows[0];
+      })
+      .catch((err) => {
+        return Promise.reject(err);
+      });
+}
+
+module.exports = {selectCommentsByArticleId, createComment};
