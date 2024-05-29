cc8fe0f03dc562076c4e3ba5e69be6c8a16850bc
GANDHIX <mattdixon2000@hotmail.com>
2024-05-29 14:44:32 +0100

    Formatting

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -12,22 +12,22 @@ afterAll(() => db.end());
 describe("GET /api", () => {
   test("Returns a valid endpoints object", () => {
     return request(app).get('/api').expect(200)
-      .then(({body: endpointsObj}) => {
-        // Weakly matches the general pattern of a URL method followed by a path
-        const pathPattern = /^(GET|POST|PATCH|DELETE) \/[.]*/;
-        
-        for(const [path, pathObj] of Object.entries(endpointsObj)) {
-          expect(pathPattern.test(path)).toBe(true);
+        .then(({body: endpointsObj}) => {
+          // Weakly matches the general pattern of a URL method followed by a path
+          const pathPattern = /^(GET|POST|PATCH|DELETE) \/[.]*/;
+          
+          for(const [path, pathObj] of Object.entries(endpointsObj)) {
+            expect(pathPattern.test(path)).toBe(true);
 
-          // Contains mandatory key of 'description'
-          expect(pathObj).toMatchObject({
-            description: expect.any(String)
-          });
+            // Contains mandatory key of 'description'
+            expect(pathObj).toMatchObject({
+              description: expect.any(String)
+            });
 
-          // If optional 'queries' key exists, check it is an array
-          if (pathObj.queries)
-            expect(Array.isArray(pathObj.queries)).toBe(true);
-        }
-      });
+            // If optional 'queries' key exists, check it is an array
+            if (pathObj.queries)
+              expect(Array.isArray(pathObj.queries)).toBe(true);
+          }
+        });
   });
 });
