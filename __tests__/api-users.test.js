750e8c01f9f136931dc1871782c90705b1b244f4
GANDHIX <mattdixon2000@hotmail.com>
2024-05-30 22:50:41 +0100

    Added tests for GET /users/:username

--- __tests__/api-users.test.js
+++ __tests__/api-users.test.js
@@ -13,7 +13,7 @@ describe("GET /api/users", () => {
   test("Returns 200 OK with an array of all the users", () => {
     return request(app).get('/api/users').expect(200)
         .then(({body}) => {
-          const users = body.users;
+          const {users} = body;
 
           expect(users.length).toBe(4);
 
@@ -27,6 +27,29 @@ describe("GET /api/users", () => {
             // 'avatar_url' key is a valid URL
             expect(() => {new URL(user.avatar_url)}).not.toThrow(Error);
           });
+        });
+  });
+});
+
+describe("GET /api/users/:username", () => {
+  test("Returns 200 OK with the correct user", () => {
+    return request(app).get('/api/users/butter_bridge').expect(200)
+        .then(({body}) => {
+          const {user} = body;
+
+          expect(user).toMatchObject({
+            username: 'butter_bridge',
+            name: 'jonny',
+            avatar_url: 'https://www.healthytherapies.com/wp-content/uploads/' 
+                + '2016/06/Lime3.jpg'
+          });
+        });
+  });
+
+  test("Returns a 404 when the user does not exist", () => {
+    return request(app).get('/api/users/unknown_user').expect(404)
+        .then(({body}) => {
+          expect(body.msg).toBe('Resource not found!');
         });
   });
 });
