eba7ccaf083e27e5ecef0399f52e396a8989404a
GANDHIX <mattdixon2000@hotmail.com>
2024-05-30 22:22:37 +0100

    Added /articles routes

--- routes/api-router.js
+++ routes/api-router.js
@@ -5,11 +5,13 @@ const apiController = require('../controllers/api.controller.js');
 const topicsRouter = require('./topics-router.js');
 const usersRouter = require('./users-router.js');
 const commentsRouter = require('./comments-router.js');
+const articlesRouter = require('./articles-router.js');
 
 apiRouter.get('/', apiController.getEndpoints);
 
 apiRouter.use('/topics', topicsRouter);
 apiRouter.use('/users', usersRouter);
 apiRouter.use('/comments', commentsRouter);
+apiRouter.use('/articles', articlesRouter);
 
 module.exports = apiRouter;
