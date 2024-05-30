819918ea303b41d40c08d81cd0a0297b35e18c05
GANDHIX <mattdixon2000@hotmail.com>
2024-05-30 22:30:04 +0100

    Formatting

--- routes/articles-router.js
+++ routes/articles-router.js
@@ -8,18 +8,8 @@ articlesRouter.route('/:article_id')
     .get(articlesController.getArticleById)
     .patch(articlesController.patchArticleById);
 
-// articlesRouter.get('/:article_id', articlesController.getArticleById);
-
-// articlesRouter.patch('/:article_id', articlesController.patchArticleById);
-
 articlesRouter.route('/:article_id/comments')
     .get(articlesController.getCommentsByArticleId)
     .post(articlesController.postCommentByArticleId);
-
-// articlesRouter.get('/:article_id/comments', 
-//     articlesController.getCommentsByArticleId);
-
-// articlesRouter.post('/:article_id/comments',
-//     articlesController.postCommentByArticleId);
 
 module.exports = articlesRouter;
