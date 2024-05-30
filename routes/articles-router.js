7546bd18eddf0034a591fe1383080498c2d6cb80
GANDHIX <mattdixon2000@hotmail.com>
2024-05-30 22:28:28 +0100

    Refactored routes to use route() method

--- routes/articles-router.js
+++ routes/articles-router.js
@@ -4,14 +4,22 @@ const articlesController = require('../controllers/articles.controller.js');
 
 articlesRouter.get('/', articlesController.getArticles);
 
-articlesRouter.get('/:article_id', articlesController.getArticleById);
+articlesRouter.route('/:article_id')
+    .get(articlesController.getArticleById)
+    .patch(articlesController.patchArticleById);
 
-articlesRouter.patch('/:article_id', articlesController.patchArticleById);
+// articlesRouter.get('/:article_id', articlesController.getArticleById);
 
-articlesRouter.get('/:article_id/comments', 
-    articlesController.getCommentsByArticleId);
+// articlesRouter.patch('/:article_id', articlesController.patchArticleById);
 
-articlesRouter.post('/:article_id/comments',
-    articlesController.postCommentByArticleId);
+articlesRouter.route('/:article_id/comments')
+    .get(articlesController.getCommentsByArticleId)
+    .post(articlesController.postCommentByArticleId);
+
+// articlesRouter.get('/:article_id/comments', 
+//     articlesController.getCommentsByArticleId);
+
+// articlesRouter.post('/:article_id/comments',
+//     articlesController.postCommentByArticleId);
 
 module.exports = articlesRouter;
