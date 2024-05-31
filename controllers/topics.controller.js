9cb7e4868fad371e6406d25df52b0f2a03912077
GANDHIX <mattdixon2000@hotmail.com>
2024-05-31 17:02:16 +0100

    Added postTopic() function

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -2,7 +2,7 @@ const logger = require('../logger/logger.js');
 const topicsModel = require('../models/topics.model.js');
 
 const getTopics = (req, res, next) => {
-  logger.debug("In getTopics() in topics.controller");
+  logger.debug(`In getTopics() in topics.controller`);
 
   topicsModel.selectTopics()
       .then((topics) => {
@@ -13,4 +13,19 @@ const getTopics = (req, res, next) => {
       });
 };
 
-module.exports = {getTopics};
+// RETURN 409 IF ALREADY EXISTS
+const postTopic = (req, res, next) => {
+  logger.debug(`In postTopic() in topics.controller`);
+
+  const {slug, description} = req.body;
+
+  topicsModel.createTopic(slug, description)
+      .then((topic) => {
+        res.status(200).send({topic});
+      })
+      .catch((err) => {
+        next(err);
+      });
+};
+
+module.exports = {getTopics, postTopic};
