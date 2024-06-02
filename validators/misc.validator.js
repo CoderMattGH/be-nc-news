74b679c3ed56756226a2719c8d314ab27fa649a4
GANDHIX <mattdixon2000@hotmail.com>
2024-06-02 20:48:05 +0100

    Added misc validators

---
+++ validators/misc.validator.js
@@ -0,0 +1,19 @@
+const logger = require('../logger/logger.js');
+
+const validateId = (id) => {
+  logger.debug(`In validateId in misc.validator`);
+
+  if ((typeof id !== 'number') || (!id instanceof Number) || isNaN(id))
+    return {valid: false, msg: 'ID must be a number!'};    
+
+  if (!Number.isInteger(id))
+    return {valid: false, msg: 'ID must be an integer!'};
+
+  // Id cannot be negative
+  if (id < 0)
+    return {valid: false, msg: 'ID cannot be negative!'};
+
+  return {valid: true};  
+};
+
+module.exports = {validateId};
