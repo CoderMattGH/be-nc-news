f98f4617df477d6ee52390f446d9af56877c1e7c
GANDHIX <mattdixon2000@hotmail.com>
2024-06-02 23:33:46 +0100

    Moved validation to the model

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,20 +1,10 @@
 const logger = require('../logger/logger.js');
 const commentsModel = require('../models/comments.model.js');
-const commentValidator = require('../validators/comment.validator.js');
-const miscValidator = require('../validators/misc.validator.js');
 
 const deleteCommentById = (req, res, next) => {
   logger.debug(`In deleteCommentById() in comments.controller`);
 
   const commentId = Number(req.params.comment_id);
-
-  // Validate ID
-  const idValObj = miscValidator.validateId(commentId);
-  if (!idValObj.valid) {
-    next({status: 400, msg: idValObj.msg});
-
-    return;
-  }  
 
   logger.info(`Deleting comment where comment_id:${commentId}`);
 
@@ -32,22 +22,6 @@ const patchCommentById = (req, res, next) => {
 
   let commentId = Number(req.params.comment_id);
   let incVotes = Number(req.body.inc_votes);
-
-  // Validate ID
-  const idValObj = miscValidator.validateId(commentId);
-  if (!idValObj.valid) {
-    next({status: 400, msg: idValObj.msg});
-
-    return;
-  }
-
-  // Validate votes
-  const voteValObj = commentValidator.validateVote(incVotes);
-  if (!voteValObj.valid) {
-    next({status: 400, msg: voteValObj.msg});
-
-    return;
-  }
 
   commentsModel.increaseVoteByCommentId(commentId, incVotes)
       .then((comment) => {
