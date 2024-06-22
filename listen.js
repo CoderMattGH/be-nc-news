88a3969e62d899b24a9fc10bfa60d068ad73d90a
GANDHIX <mattdixon2000@hotmail.com>
2024-06-22 22:04:19 +0100

    Added ability tobind to a different IP address

--- listen.js
+++ listen.js
@@ -3,7 +3,8 @@ const logger = require(`./logger/logger.js`);
 const app = require('./app.js');
 
 const {PORT = 9090} = process.env;
+const {IP_ADD = '127.0.0.1'} = process.env;
 
 app.listen(PORT, '127.0.0.1', () => {
-  logger.info(`Successfully listening on port ${PORT} on 127.0.0.1`);
+  logger.info(`Successfully listening on port ${PORT} on ${IP_ADD}`);
 });
