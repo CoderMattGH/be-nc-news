b6613708c2ef51aec5310b65df75721796525816
GANDHIX <mattdixon2000@hotmail.com>
2024-06-22 15:23:06 +0100

    Updated IP to loopback

--- listen.js
+++ listen.js
@@ -4,6 +4,6 @@ const app = require('./app.js');
 
 const {PORT = 9090} = process.env;
 
-app.listen(PORT, () => {
-  logger.info(`Successfully listening on port ${PORT}`);
+app.listen(PORT, '127.0.0.1', () => {
+  logger.info(`Successfully listening on port ${PORT} on 127.0.0.1`);
 });
